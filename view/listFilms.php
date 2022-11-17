3da19fec9fb0fdf231d387cdeceedace26c38996
mayados <mariannedos.cours@gmail.com>
2022-11-17 17:01:46 +0100

    add

--- view/listFilms.php
+++ view/listFilms.php
@@ -13,7 +13,8 @@
             <?php
                 foreach($requete->fetchAll() as $film) { ?>
                     <tr>
-                        <td><?=$film["titre"] ?></td>
+                        
+                        <td><a href="index.php?action=detailFilm&id='<?= $film['id_film']?>'"><?=$film["titre"] ?></a></td>
                         <td><?=$film["date_sortie"] ?></td>
                     </tr>
             <?php } ?>   
@@ -31,3 +32,4 @@
      require "view/template.php";
 
     ?> 
+
