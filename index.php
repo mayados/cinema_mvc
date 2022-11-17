bc475339bd0abec1f9d4cd84f0e30101ac898589
mayados <mariannedos.cours@gmail.com>
2022-11-17 15:41:14 +0100

    add case listRealisateurs and listActeurs

--- index.php
+++ index.php
@@ -16,8 +16,27 @@
         /* Pour l'action reçue, on traite le case qui correspond au nom de l'action  */
         switch ($_GET["action"]){
             case "listFilms" : $ctrlCinema->listFilms(); break;
+            case "listRealisateurs" : $ctrlCinema->listRealisateurs(); break;
             case "listActeurs" : $ctrlCinema->listActeurs(); break;
         }
     }
 
 ?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<a href="index.php?action=listFilms">Voir les films</a>
+<a href="index.php?action=listRealisateurs">Voir les réalisateurs</a>
+<a href="index.php?action=listActeurs">Voir les acteurs</a>
+</body>
+</html>
