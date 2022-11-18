cbcd4b3835be4d60aabaf7894f1d7e439e93a1c1
mayados <115099504+mayados@users.noreply.github.com>
2022-11-18 11:53:45 +0100

    create variables to stock informations + add these variables in $titreSecondaire

--- view/detailRealisateur.php
+++ view/detailRealisateur.php
@@ -1,25 +1,22 @@
-<?php ob_start();?>
-
+<?php ob_start(); 
+?>
 <p>Ce réalisateur a réalisé <?= $requete->rowCount() ?> films</p>
 
     <table>
         <thead>
             <tr>
-                <th>NOM</th>
-                <th>PRENOM</th>
-                <th>SEXE</th>
-                <th>NAISSANCE</th>
-                <th>TITRE FILMS</th>
+                <th>REALISATIONS</th>
             </tr>
         </thead>
         <tbody>
             <?php
-                foreach($requete->fetchAll() as $realisateur) { ?>
+                foreach($requete->fetchAll() as $realisateur) { 
+                    $realisateurNom = $realisateur["nom"];
+                    $realisateurPrenom = $realisateur["prenom"]; 
+                    $realisateurSexe = $realisateur["sexe"];
+                    $realisateurNaissance = $realisateur["date_naissance"];
+            ?>
                     <tr>
-                        <td><?=$realisateur["nom"] ?></td>
-                        <td><?=$realisateur["prenom"] ?></td>
-                        <td><?=$realisateur["sexe"] ?></td>
-                        <td><?=$realisateur["date_naissance"] ?></td>
                         <td><?=$realisateur["titre"] ?></td>
                     </tr>
             <?php } ?>   
@@ -30,7 +27,7 @@
     
     /* On stocke dans des variables les titres qui seront affectés sur la page template */
     $titre = "informations réalisateur";
-    $titre_secondaire = "informations du realisateur";
+    $titre_secondaire = "Realisateur : ".$realisateurPrenom." ".$realisateurNom." (".$realisateurSexe.", ".$realisateurNaissance.")";
     /* ob_start et ob_get_clean permettent d'aspirer les informations qui se situent entre ces deux fonctions, ainsi, nous pourrons envoyer tout ça sur une autre page
     Ici, nous stockons tout cela dans une variable $contenu */
     $contenu = ob_get_clean();
