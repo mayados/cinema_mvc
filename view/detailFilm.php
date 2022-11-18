ce3e4974ba9ba611f14dd55082f75e600def7d70
mayados <115099504+mayados@users.noreply.github.com>
2022-11-18 12:08:35 +0100

    restructure the code

--- view/detailFilm.php
+++ view/detailFilm.php
@@ -1,21 +1,23 @@
 <?php ob_start(); ?>
 
-<p>Informations sur le film</p>
+<p>Casting du film</p>
 
     <table>
         <thead>
             <tr>
-                <th>TITRE</th>
-                <th>DATE DE SORTIE</th>
+                <th>NOM</th>
+                <th>PRENOM</th>
+                <th>ROLE</th>
             </tr>
         </thead>
         <tbody>
             <?php
-                foreach($requete->fetchAll() as $film) { ?>
+                foreach($requete->fetchAll() as $film) { 
+                    $titreFilm = $film["titre"];
+                    $filmSortie = $film["date_sortie"];
+                    $filmDuree = $film["duree"];
+                    ?>
                     <tr>
-                        <td><?=$film["titre"] ?></td>
-                        <td><?=$film["date_sortie"] ?></td>
-                        <td><?=$film["duree"] ?></td>
                         <td><?=$film["nom"] ?></td>
                         <td><?=$film["prenom"] ?></td>
                         <td><?=$film["nom_role"] ?></td>
@@ -28,7 +30,7 @@
     
     /* On stocke dans des variables les titres qui seront affectés sur la page template */
     $titre = "Liste des films";
-    $titre_secondaire = "Liste des films";
+    $titre_secondaire = "Film : ".$titreFilm." (Durée : ".$filmDuree." / Date de sortie : ".$filmSortie.")";
     /* ob_start et ob_get_clean permettent d'aspirer les informations qui se situent entre ces deux fonctions, ainsi, nous pourrons envoyer tout ça sur une autre page
     Ici, nous stockons tout cela dans une variable $contenu */
     $contenu = ob_get_clean();
