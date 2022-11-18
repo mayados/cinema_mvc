6048346fc9d124ee7800690f6dde812eabb682a9
mayados <115099504+mayados@users.noreply.github.com>
2022-11-18 10:01:50 +0100

    add link to get the id of the realisateur clicked

--- view/listRealisateurs.php
+++ view/listRealisateurs.php
@@ -5,15 +5,15 @@
     <table>
         <thead>
             <tr>
-                <th>TITRE</th>
-                <th>DATE DE SORTIE</th>
+                <th>NOM</th>
+                <th>PRENOM</th>
             </tr>
         </thead>
         <tbody>
             <?php
                 foreach($requete->fetchAll() as $realisateur) { ?>
                     <tr>
-                        <td><?=$realisateur["nom"] ?></td>
+                        <td><a href="index.php?action=detailRealisateur&id=<?= $realisateur['id_realisateur']?>"><?=$realisateur["nom"] ?></td>
                         <td><?=$realisateur["prenom"] ?></td>
                     </tr>
             <?php } ?>   
