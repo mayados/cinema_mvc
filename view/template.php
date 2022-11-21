780a52c078b752dcc14e1a40bee9e3cfe812a733
mayados <115099504+mayados@users.noreply.github.com>
2022-11-21 20:32:18 +0100

    add link php for css + delete #contenu div

--- view/template.php
+++ view/template.php
@@ -4,26 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/<?=$lienCss?>.css">
     <title><?= $titre ?></title>
 </head>
 <body>
     <nav>
-        <a href="index.php?action=listFilms">Voir les films</a>
-        <a href="index.php?action=listRealisateurs">Voir les réalisateurs</a>
-        <a href="index.php?action=listActeurs">Voir les acteurs</a>
-        <a href="index.php?action=listGenres">Voir les genres</a>  
-        <a href="index.php?action=listRoles">Voir les rôles</a> 
+        <a href="index.php?action=listFilms">Films</a>
+        <a href="index.php?action=listRealisateurs">Réalisateurs</a>
+        <a href="index.php?action=listActeurs">Acteurs</a>
+        <a href="index.php?action=listGenres">Genres</a>  
+        <a href="index.php?action=listRoles">Rôles</a> 
         <a href="index.php?action=ajoutActeur">Ajouter un acteur</a>   
         <a href="index.php?action=ajoutRealisateur">Ajouter un réalisateur</a>       
         <a href="index.php?action=ajoutGenre">Ajouter un genre</a>      
         <a href="index.php?action=ajoutFilm">Ajouter un film</a>     
     </nav>
     <div id="container-page">
-        <div id="contenu">
-            <h1>PDO CINEMA</h1>
             <h2><?= $titre_secondaire ?></h2>
             <?= $contenu ?>
-        </div>
     </div>
 </body>
 </html>
