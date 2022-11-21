57ee9cc61636e378df0ca8272fd94f3fd66490f6
mayados <115099504+mayados@users.noreply.github.com>
2022-11-21 20:32:31 +0100

    add div

--- view/ajoutFilm.php
+++ view/ajoutFilm.php
@@ -2,48 +2,56 @@
 
         <form action="index.php?action=insertFilm" method="post">
 
-            <label for="nom">Nom du film :</label>
-                <input type="text" id="nom" name="nom" placeholder="exemple : Elephant Man" required>
+            <div class="form-champ">
+                <label for="nom">Nom du film :</label>
+                    <input type="text" id="nom" name="nom" placeholder="exemple : Elephant Man" required>                
+            </div>
+            <div class="form-champ">
+                <label for="duree">Durée :</label>
+                    <input type="text" id="duree" name="duree" placeholder="exemple : 02:29:00" required>                
+            </div>
+            <div class="form-champ">
+                <label for="dateSortie">Date de sortie :</label>
+                    <input type="text" id="dateSortie" name="dateSortie" placeholder="2022-11-18" required>                
+            </div>
+            <div class="form-champ">
+                <label for="synopsis">Synopsis :</label>
+                    <textarea name="synopsis" id="synopsis" placeholder="Résumé du film" required></textarea>                
+            </div>
+            <div class="form-champ">
+                <label for="affiche">Affiche :</label>
+                    <textarea name="affiche" id="affiche" placeholder="URL de l'affiche" redquired></textarea>                
+            </div>
+            <div class="form-champ">
+                <label for="realisateur">Choisissez un réalisateur:</label>
 
-            <label for="duree">Durée :</label>
-                <input type="text" id="duree" name="duree" placeholder="exemple : 02:29:00" required>
+                <select name="realisateur" id="realisateur">
+                    <?php
+                        foreach($requeteRealisateurs->fetchAll() as $realisateur) { 
+                                $real = $realisateur["nom"]." ".$realisateur["prenom"];
+                                $realNom = $realisateur["nom"];
+                                $realId = $realisateur["id_realisateur"];
+                            ?>
 
-            <label for="dateSortie">Date de sortie :</label>
-                <input type="text" id="dateSortie" name="dateSortie" placeholder="2022-11-18" required>
+                            <option value="<?=$realId ?>" name="<?=$realNom ?>"><?=$real ?></option>
+                    <?php } ?>   
+                </select>                
+            </div>
+            <div class="form-champ">
+                <label for="genre">Choisissez le(s) genre(s):</label>
 
-            <label for="synopsis">Synopsis :</label>
-                <textarea name="synopsis" id="synopsis" placeholder="Résumé du film" required></textarea>
+                <select  name="genre[]" id="genre" multiple>
+                    <?php
+                        foreach($requeteGenres->fetchAll() as $genre) { 
+                                $libelle = $genre["libelle"];
+                                $idGenre = $genre["id_genre"];
+                            ?>
+                            <option value="<?= $idGenre ?>" name="<?= $libelle ?>"><?= $libelle ?></option>
+                    <?php } ?>   
+                </select>                
+            </div>
 
-            <label for="affiche">Affiche :</label>
-                <textarea name="affiche" id="affiche" placeholder="URL de l'affiche" redquired></textarea>
-
-            <label for="realisateur">Choisissez un réalisateur:</label>
-
-            <select name="realisateur" id="realisateur">
-                <?php
-                    foreach($requeteRealisateurs->fetchAll() as $realisateur) { 
-                             $real = $realisateur["nom"]." ".$realisateur["prenom"];
-                             $realNom = $realisateur["nom"];
-                             $realId = $realisateur["id_realisateur"];
-                        ?>
-
-                        <option value="<?=$realId ?>" name="<?=$realNom ?>"><?=$real ?></option>
-                <?php } ?>   
-            </select>
-
-            <label for="genre">Choisissez le(s) genre(s):</label>
-
-            <select  name="genre[]" id="genre" multiple>
-                <?php
-                    foreach($requeteGenres->fetchAll() as $genre) { 
-                            $libelle = $genre["libelle"];
-                            $idGenre = $genre["id_genre"];
-                        ?>
-                        <option value="<?= $idGenre ?>" name="<?= $libelle ?>"><?= $libelle ?></option>
-                <?php } ?>   
-            </select>
-
-                <input name="submit" type="submit" value="Envoyer">
+            <input name="submit" type="submit" value="Envoyer">
 
         </form>
 
@@ -52,6 +60,7 @@
     /* On stocke dans des variables les titres qui seront affectés sur la page template */
     $titre = "Ajouter un film";
     $titre_secondaire = "Formulaire : ajouter un film";
+    $lienCss = "ajoutFilm";
     /* ob_start et ob_get_clean permettent d'aspirer les informations qui se situent entre ces deux fonctions, ainsi, nous pourrons envoyer tout ça sur une autre page
     Ici, nous stockons tout cela dans une variable $contenu */
     $contenu = ob_get_clean();
