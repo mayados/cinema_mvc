ada0d8a2e1cb48090bbfb91cbeee254f9304eda6
mayados <mariannedos.cours@gmail.com>
2022-11-22 16:48:45 +0100

    create container-infos and container-casting

--- view/detailFilm.php
+++ view/detailFilm.php
@@ -11,41 +11,52 @@ foreach($requeteFilm->fetchAll() as $film) {
 
 ?>
 
-    <img src="<?= $affiche ?>" alt="affiche du film">
-    <h4>synopsis :</h4>
-    <p><?= $synopsis ?></p>
+    <div id="container-infos">
+            <div id="synopsis">
+                <h4>Synopsis :</h4>           
+                <p><?= $synopsis ?></p>        
+            </div>    
+        <img src="<?= $affiche ?>" alt="affiche du film">          
+    </div>
 
+    <div id="container-bottom">
+        <div id="infos-film">
+                <p>Durée : <?= $filmDuree ?></p>
+                <p>Date de sortie : <?= $filmSortie?></p>
+                <p>Note : <?= $note ?> /5</p>
+            </div> 
+        <div id="container-casting">
+            <?php 
+                if( $requeteCasting->rowCount() > 0){ 
+                /* On affiche le code HTML / php ci-dessous uniquement s'il y a au moins un casting */        
+            ?>
+
+            <h4>Casting</h4>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ACTEUR</th>
+                            <th>ROLE</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            foreach($requeteCasting->fetchAll() as $casting) { ?>
+                                    <td><?=$casting["prenom"]." ".$casting["nom"] ?></td>
+                                    <td><?=$casting["nom_role"] ?></td>
+                        <?php } ?>   
+                    </tbody>
+                </table>               
+        </div>
+              
+    </div>
  
-<?php 
-    if( $requeteCasting->rowCount() > 0){ 
-    /* On affiche le code HTML / php ci-dessous uniquement s'il y a au moins un casting */        
-?>
-
-<p>Casting du film</p>
-
-    <table>
-        <thead>
-            <tr>
-                <th>ACTEUR</th>
-                <th>ROLE</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                foreach($requeteCasting->fetchAll() as $casting) { ?>
-                        <td><?=$casting["nom"]." ".$casting["prenom"] ?></td>
-                        <td><?=$casting["nom_role"] ?></td>
-            <?php } ?>   
-        </tbody>
-    </table>
-
-
-
     <?php
     }
     /* On stocke dans des variables les titres qui seront affectés sur la page template */
     $titre = "Liste des films";
-    $titre_secondaire = "Film : ".$titreFilm." (Durée : ".$filmDuree." / Date de sortie : ".$filmSortie." / Note : ".$note."/5)";
+    $titre_secondaire = $titreFilm;
     $lienCss = "detailFilm";
     /* ob_start et ob_get_clean permettent d'aspirer les informations qui se situent entre ces deux fonctions, ainsi, nous pourrons envoyer tout ça sur une autre page
     Ici, nous stockons tout cela dans une variable $contenu */
