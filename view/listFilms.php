6e7aec0abbd1adf83c97599746b811795e08fe24
mayados <mariannedos.cours@gmail.com>
2022-11-22 11:19:18 +0100

    add id compteur and elements-link

--- view/listFilms.php
+++ view/listFilms.php
@@ -1,6 +1,6 @@
 <?php ob_start(); ?>
 
-<p>Il y a <?= $requete->rowCount() ?> films</p>
+<p id="compteur">Il y a <?= $requete->rowCount() ?> films</p>
 
     <table>
         <thead>
@@ -14,7 +14,7 @@
                 foreach($requete->fetchAll() as $film) { ?>
                     <tr>
                         
-                        <td><a href="index.php?action=detailFilm&id=<?= $film['id_film']?>"><?=$film["titre"] ?></a></td>
+                        <td><a id='elements-link' href="index.php?action=detailFilm&id=<?= $film['id_film']?>"><?=$film["titre"] ?></a></td>
                         <td><?=$film["date_sortie"] ?></td>
                     </tr>
             <?php } ?>   
@@ -26,6 +26,7 @@
     /* On stocke dans des variables les titres qui seront affectés sur la page template */
     $titre = "Liste des films";
     $titre_secondaire = "Liste des films";
+    $lienCss = "listFilms";
     /* ob_start et ob_get_clean permettent d'aspirer les informations qui se situent entre ces deux fonctions, ainsi, nous pourrons envoyer tout ça sur une autre page
     Ici, nous stockons tout cela dans une variable $contenu */
     $contenu = ob_get_clean();
