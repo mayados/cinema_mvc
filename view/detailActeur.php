941053e8e1f14c004b79e506cc08bb5dd1af4dac
mayados <115099504+mayados@users.noreply.github.com>
2022-11-25 14:20:22 +0100

    modify $titreSecondaire

--- view/detailActeur.php
+++ view/detailActeur.php
@@ -43,7 +43,7 @@
     
     /* On stocke dans des variables les titres qui seront affectés sur la page template */
     $titre = "informations acteur";
-    $titre_secondaire = "Acteur : ".$acteurNom." ".$acteurPrenom;
+    $titre_secondaire = $acteurPrenom." ".$acteurNom;
     $lienCss = "detailPersonne";
     /* ob_start et ob_get_clean permettent d'aspirer les informations qui se situent entre ces deux fonctions, ainsi, nous pourrons envoyer tout ça sur une autre page
     Ici, nous stockons tout cela dans une variable $contenu */
