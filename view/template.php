caf07f16067334a649d72cd40427daba33e13d12
mayados <115099504+mayados@users.noreply.github.com>
2022-11-25 16:24:15 +0100

    add header and button (burger menu)

--- view/template.php
+++ view/template.php
@@ -9,33 +9,41 @@
     <title><?= $titre ?></title>
 </head>
 <body>
-    <nav>
-        <ul id="items-menu">
-            <li><a class="nav-link" href="index.php?action=listFilms">Films</a></li>
-            <span>|</span>
-            <li><a class="nav-link" href="index.php?action=listRealisateurs">Réalisateurs</a></li>
-            <span>|</span>
-            <li><a class="nav-link" href="index.php?action=listActeurs">Acteurs</a></li>
-            <span>|</span>
-            <li><a class="nav-link" href="index.php?action=listGenres">Genres</a></li>  
-            <span>|</span>
-            <li><a class="nav-link" href="index.php?action=listRoles">Rôles</a></li>
-            <span>|</span>
-            <li id="admin"><a href="#" class="nav-link">Admin</a>
-                <ul id="sous-menu">
-                    <li><a href="index.php?action=ajoutActeur">Ajouter un acteur</a></li>
-                    <li><a href="index.php?action=ajoutRealisateur">Ajouter un réalisateur</a></li>  
-                    <li><a href="index.php?action=ajoutGenre">Ajouter un genre</a></li>     
-                    <li><a href="index.php?action=ajoutFilm">Ajouter un film</a></li>     
-                    <li><a href="index.php?action=ajoutRole">Ajouter un rôle</a></li>   
-                    <li><a href="index.php?action=ajoutCasting">Ajouter un Casting</a></li>  
-                </ul>     
-            </li>                
-        </ul>
-    </nav>
+    <header>
+        <nav id="menu-navigation">
+            <ul id="items-menu">
+                <li><a class="nav-link" href="index.php?action=listFilms">Films</a></li>
+                <span>|</span>
+                <li><a class="nav-link" href="index.php?action=listRealisateurs">Réalisateurs</a></li>
+                <span>|</span>
+                <li><a class="nav-link" href="index.php?action=listActeurs">Acteurs</a></li>
+                <span>|</span>
+                <li><a class="nav-link" href="index.php?action=listGenres">Genres</a></li>  
+                <span>|</span>
+                <li><a class="nav-link" href="index.php?action=listRoles">Rôles</a></li>
+                <span>|</span>
+                <li id="admin"><a href="#" class="nav-link">Admin</a>
+                    <ul id="sous-menu">
+                        <li><a href="index.php?action=ajoutActeur">Ajouter un acteur</a></li>
+                        <li><a href="index.php?action=ajoutRealisateur">Ajouter un réalisateur</a></li>  
+                        <li><a href="index.php?action=ajoutGenre">Ajouter un genre</a></li>     
+                        <li><a href="index.php?action=ajoutFilm">Ajouter un film</a></li>     
+                        <li><a href="index.php?action=ajoutRole">Ajouter un rôle</a></li>   
+                        <li><a href="index.php?action=ajoutCasting">Ajouter un Casting</a></li>  
+                    </ul>     
+                </li>                
+            </ul>
+            <button class="burger">
+                <span class="bar-burger"></span>
+            </button>
+        </nav>        
+    </header>
+
+
     <div id="container-page">
         <h2><?= $titre_secondaire ?></h2>
         <?= $contenu ?>
     </div>
+    <script src ="./public/js/script.js"></script>
 </body>
 </html>
